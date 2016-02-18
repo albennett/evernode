aad8c3cf729a9899442cefc4ce2dc975f09e09d5
Amy Lee <amyleeannebennett@gmail.com>
2016-02-18 10:35:13 -0600

    new note form

--- server.js
+++ server.js
@@ -7,11 +7,22 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
 
-app.listen(port, () =>{
+app.post('/notes', (req, res) => {
+  console.log("req.body",req.body);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
-})
+});
