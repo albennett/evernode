67a0792b74ac14cee6f19c2913a4e3ea56fa27df
Amy Lee <amyleeannebennett@gmail.com>
2016-02-18 11:19:54 -0600

    redirects to note view after creation

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 const Note = mongoose.model('Notes', mongoose.Schema({
   title: String,
   text: String
 }));
 
 app.set('view engine', 'jade');
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
@@ -24,12 +26,20 @@ app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
 
+app.get('/notes/:id', (req, res) => {
+  Note.findById(req.params.id, (err, note) => {
+    if (err) throw err;
+
+    res.render('show-note', {note: note});
+  });
+});
+
 app.post('/notes', (req, res) => {
   Note.create(req.body, (err, note) => {
     if (err) throw err;
-    console.log("note", note);
-    res.redirect('/');
-  })
+
+    res.redirect(`/notes/${note._id}`);
+  });
 });
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
