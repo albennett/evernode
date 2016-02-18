b700605c67b3841744c60acb37127a45477a3085
Amy Lee <amyleeannebennett@gmail.com>
2016-02-18 10:53:10 -0600

    new notes are saving to database

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
 
 app.set('view engine', 'jade');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.get('/', (req, res) => {
   res.send('Server Running');
@@ -19,10 +25,17 @@ app.get('/notes/new', (req, res) => {
 });
 
 app.post('/notes', (req, res) => {
-  console.log("req.body",req.body);
-  res.redirect('/');
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log("note", note);
+    res.redirect('/');
+  })
 });
 
-app.listen(port, () => {
-  console.log(`Evernode server running on port: ${port}`);
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
 });
