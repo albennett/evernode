822cc631dcfc2cbed91357ff11c740fc61525a9e
Amy Lee <amyleeannebennett@gmail.com>
2016-02-18 11:37:36 -0600

    refactored routes and models

--- server.js
+++ server.js
@@ -3,14 +3,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const note = require('./routes/note');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const Note = mongoose.model('Notes', mongoose.Schema({
-  title: String,
-  text: String
-}));
 
 app.set('view engine', 'jade');
 
@@ -22,25 +18,7 @@ app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  });
-});
-
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-
-    res.redirect(`/notes/${note._id}`);
-  });
-});
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
